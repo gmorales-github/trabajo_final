8ec0159bbb99c54bccde8726ef3d3b30a687731a
gmorales-github <gustavom.morales@gmail.com>
2024-06-21 04:09:13 -0300

    Se agrega tablas de prueba

--- gui/main_menu.py
+++ gui/main_menu.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
  
 def main_menu():
     # Crear la ventana principal del menu
@@ -6,7 +7,21 @@ def main_menu():
     window.title("Menu principal")
     window.geometry("600x600")
     window.resizable(width=False, height=False)
+    window['bg'] = '#fb0'
 
+
+    # Cargo las columnas 
+    columns = ("id_usuario", "nombre_usuario", "contrasenia")
+    
+    tv = ttk.Treeview(window, columns = columns, show="headings")
+    tv.heading ('id_usuario', text='id usuario', anchor=tk.CENTER)
+    tv.heading ('nombre_usuario', text="nombre usuario", anchor=tk.CENTER)
+    tv.heading ('contrasenia', text="contraseña", anchor=tk.CENTER)
+
+    tv.insert ('', tk.END, values = ('1', 'admin1', 'adminpass1'))
+    tv.insert ('', tk.END, values = ('2', 'usuario1', 'adminpass1'))
+    tv.insert ('', tk.END, values = ('3', 'usuario2', 'adminpass1'))
+    tv.grid (row=0, column=2, padx=10, pady=50)
 
     # Ejecutar la aplicación
     window.mainloop()
