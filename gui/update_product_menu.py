6776e969403cf25008ce0778acbe4c0bf00ccc1e
gmorales-github <gustavom.morales@gmail.com>
2024-06-26 20:58:05 -0300

    Se agrega id en el form de edición

--- gui/update_product_menu.py
+++ gui/update_product_menu.py
@@ -41,15 +41,23 @@ def update_product(tv, data):
     window['bg'] = '#b0c6ff'
 
     # Crear y colocar etiquetas y entradas de texto
+    label_productid = tk.Label(window, text="ID producto:")
+    label_productid.grid(row=0, column=0, padx=5, pady=10)
+
+    entry_productid = tk.Entry(window)
+    entry_productid.insert(0, product["id_producto"])    
+    entry_productid.grid(row=0, column=1, padx=5, pady=10)
+    entry_productid.config(state='disabled')
+
     label_productname = tk.Label(window, text="Nombre del producto:")
-    label_productname.grid(row=0, column=0, padx=5, pady=50)
+    label_productname.grid(row=1, column=0, padx=5, pady=50)
 
     entry_productname = tk.Entry(window)
     entry_productname.insert(0, product["nombre_producto"])
-    entry_productname.grid(row=0, column=1, padx=5, pady=50)
+    entry_productname.grid(row=1, column=1, padx=5, pady=50)
 
     label_price = tk.Label(window, text="Precio")
-    label_price.grid(row=1, column=0, padx=5, pady=5)
+    label_price.grid(row=2, column=0, padx=5, pady=5)
 
     # TODO: Corregir el error en la validación y casting de datos
     #entry_price_var = tk.StringVar()
@@ -57,23 +65,23 @@ def update_product(tv, data):
     #entry_price = tk.Entry(window, textvariable=entry_price_var, validate="key", validatecommand=(validate_price_command, '%S'))
     entry_price = tk.Entry(window)
     entry_price.insert(0, product["precio"])
-    entry_price.grid(row=1, column=1, padx=5, pady=5)
+    entry_price.grid(row=2, column=1, padx=5, pady=5)
 
     label_description = tk.Label(window, text="Descripción")
-    label_description.grid(row=2, column=0, padx=5, pady=5)
+    label_description.grid(row=3, column=0, padx=5, pady=5)
 
     entry_description = tk.Entry(window)
     entry_description.insert(0, product["descripcion"])
-    entry_description.grid(row=2, column=1, padx=5, pady=5)
+    entry_description.grid(row=3, column=1, padx=5, pady=5)
 
     label_stock = tk.Label(window, text="Cantidad")
-    label_stock.grid(row=3, column=0, padx=5, pady=5)
+    label_stock.grid(row=4, column=0, padx=5, pady=5)
 
     entry_stock_var = tk.StringVar()
     validate_stock_command = window.register(check_integer)
     entry_stock = tk.Entry(window, textvariable=entry_stock_var, validate="key", validatecommand=(validate_stock_command, '%S'))
     entry_stock.insert(0, product["stock"])
-    entry_stock.grid(row=3, column=1, padx=5, pady=5)
+    entry_stock.grid(row=4, column=1, padx=5, pady=5)
 
     # Crear y colocar el botón de actualizar
     button_update = tk.Button(window, text="Actualizar", command=lambda: (update(id_producto=product["id_producto"],nombre_producto=entry_productname.get(), precio=entry_price.get(), descripcion=entry_description.get(), stock=entry_stock.get()), refresh_table(tv)))    
